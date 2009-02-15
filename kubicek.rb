87f272e69d8a421e879eaa9afec4283488cc97db
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2009-02-16 00:43:33 +0100

    Odstraneni nepouzivanych cest

--- kubicek.rb
+++ kubicek.rb
@@ -1,18 +1,6 @@
 require "rubygems"
 require "sinatra"
 
-get "/skoly" do
-  haml :skoly
-end
-
-get "/organy" do
-  haml :organy
-end
-
-get "/clenstvi" do
-  haml :clenstvi
-end
-
 get "/?" do
   haml :index
 end

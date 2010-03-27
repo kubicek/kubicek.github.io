8e65368467b4dd93bdaa4244030a5c02e3f6485b
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2010-03-27 04:03:08 +0100

    Změna layoutu a struktury informací, volby PSP 2010

--- kubicek.rb
+++ kubicek.rb
@@ -1,6 +1,22 @@
 require "rubygems"
 require "sinatra"
 
+get "/kdo-jsem/?" do
+  erb :about
+end
+
+get "/temata/?" do
+  erb :tags
+end
+
+get "/galerie/?" do
+  erb :gallery
+end
+
+get "/kontakt/?" do
+  erb :contact
+end
+
 get "/?" do
-  haml :index
+  erb :index
 end
